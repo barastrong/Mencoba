71a8c00d89b98c82a7bfaed5b7e3324bfe065dc3
barastrong <bintangbaraadyasta@gmail.com>
2025-03-13 19:43:26 +0700

    MencobaBaru

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -90,7 +90,7 @@ const MovieList = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Film Populer</Text>
+      <Text style={styles.header}>Film Terpopuler saat ini</Text>
       <FlatList
         data={movies}
         renderItem={renderMovieItem}
